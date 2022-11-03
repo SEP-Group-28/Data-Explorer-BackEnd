6a5cccaa6e2cde87cb32823ba28d2b7a0200434f
Thushalya <weeeratharava@gmail.com>
2022-11-03 12:12:57 +0530

    add misssed db connection

--- dbconnection.py
+++ dbconnection.py
@@ -4,8 +4,13 @@ from pymongo import MongoClient
 # load_dotenv()
 
 def connectdb():
+    
+# client = pymongo.MongoClient("mongodb+srv://thushalya:XVgpQf4Bn9qPrewc@cluster0.xxdhd7z.mongodb.net/?retryWrites=true&w=majority")
+# db = client.test
+
     # print(os.environ.get('ADMIN_ROLE'))
     DATABASE_URL=os.environ.get('DATABASE_URL')
+    # mongodb+srv://thushalya:XVgpQf4Bn9qPrewc@cluster0.xxdhd7z.mongodb.net/test
     # cluster= MongoClient(DATABASE_URL) #use for remote server
 
     cluster = MongoClient(DATABASE_URL,27017)  
