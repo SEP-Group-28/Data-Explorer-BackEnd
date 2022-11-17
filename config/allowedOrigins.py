a892ed903d4a250bc41a503ebeed279423265eba
Thiniraxz <thiniragenuka@gmail.com>
2022-11-17 18:28:46 +0530

    Update origins

--- config/allowedOrigins.py
+++ config/allowedOrigins.py
@@ -3,6 +3,12 @@ allowedOrigins = {
     'http://localhost:5173',
     'https://localhost:5173',
     'https://127.0.0.1:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
+    'http://127.0.0.1:3000',
+  
 }
 
